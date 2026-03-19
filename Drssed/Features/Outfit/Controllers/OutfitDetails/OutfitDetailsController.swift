8489d8596caf128d7535aaa20957b0f5bb315d49
David Riegel <40246197+davidriegel@users.noreply.github.com>
2026-03-19 14:07:25 +0100

    Create OutfitDetailsController

---
+++ Drssed/Features/Outfit/Controllers/OutfitDetails/OutfitDetailsController.swift
@@ -0,0 +1,14 @@
+//
+//  OutfitDetailsController.swift
+//  Drssed
+//
+//  Created by David Riegel on 19.03.26.
+//
+
+import UIKit
+import CropViewController
+import PhotosUI
+
+final class OutfitDetailsController: UIViewController {
+    
+}
