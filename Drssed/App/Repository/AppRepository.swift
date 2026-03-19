2f51f28a0acca889fa52dcccdd9eeb1f97b20af8
David Riegel <40246197+davidriegel@users.noreply.github.com>
2026-03-19 13:35:40 +0100

    Remove uneccassary comments

--- Drssed/App/Repository/AppRepository.swift
+++ Drssed/App/Repository/AppRepository.swift
@@ -11,24 +11,13 @@ final public class AppRepository {
     public static let shared = AppRepository()
     public let clothingRepository: ClothingRepository
     public let outfitRepository: OutfitRepository
-    //let userRepository: UserRepository
     
     public init(context: NSManagedObjectContext) {
         clothingRepository = ClothingRepository(context: context)
         outfitRepository = OutfitRepository(context: context)
-        //userRepository = UserRepository(context: context)
     }
 
     public convenience init() {
         self.init(context: PersistenceController.shared.container.viewContext)
     }
-
-    //App-weite UseCases:
-    /*
-    func syncAll() async throws {
-        try await clothingRepository.syncWithServerModels(/* ... */)
-        try await outfitRepository.syncWithServerModels(/* ... */)
-        try await userRepository.syncWithServerModels(/* ... */)
-    }*/
 }
-
