73bb7be6517577a91479044ab4604f0ead39adf8
David Riegel <40246197+davidriegel@users.noreply.github.com>
2026-04-30 22:59:55 +0200

    refactor: remove old PrivateUser struct and with it old files from previous versions; feat: add User struct for future implementations

--- Drssed/Features/User/Models/User.swift
+++ Drssed/Features/User/Models/User.swift
@@ -7,14 +7,12 @@
 
 import Foundation
 
-public struct PrivateUser: Codable {
+public struct User: Codable {
     let user_id: String
-    let username: String
-    let email: String
+    let is_guest: Bool
+    let created_at: Date
+    let updated_at: Date?
+    let username: String?
+    let email: String?
     let profile_picture: String
-    let created_at: String
-    let updated_at: String
-    let friends: [String]?
-    let incoming_friend_requests: [String]?
-    let outgoing_friend_requests: [String]?
 }
