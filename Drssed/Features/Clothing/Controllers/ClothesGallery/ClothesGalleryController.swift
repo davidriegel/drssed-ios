110bffcf939843873f9d4a95787a1dff0bb9daaf
David <40246197+davidriegel@users.noreply.github.com>
2026-08-03 16:03:21 +0200

    fix/gallery-selection-after-search (#21)
    * fix: wardrobe opening the wrong item after a search
    
    * fix: lookbook opening the wrong outfit after a search

--- Drssed/Features/Clothing/Controllers/ClothesGallery/ClothesGalleryController.swift
+++ Drssed/Features/Clothing/Controllers/ClothesGallery/ClothesGalleryController.swift
@@ -268,6 +268,11 @@ class ClothesGalleryController: UIViewController {
         }
     }
     
+    private func clothing(at indexPath: IndexPath) -> Clothing? {
+        guard let id = diffableDataSource.itemIdentifier(for: indexPath) else { return nil }
+        return dataSourceByID[id]
+    }
+
     func showClothingDetails(of clothing: Clothing) {
         let detailsController = ClothingDetailsController(clothing)
         detailsController.delegate = self
@@ -714,7 +719,9 @@ extension ClothesGalleryController: UICollectionViewDelegate, UICollectionViewDe
             return
         }
 
-        showClothingDetails(of: isSearching ? searchDataSource[indexPath.item] : sortedAndFilteredDataSource[indexPath.item])
+        guard let clothing = clothing(at: indexPath) else { return }
+
+        showClothingDetails(of: clothing)
     }
     
     func didUploadClothing(_ clothing: Clothing) {
